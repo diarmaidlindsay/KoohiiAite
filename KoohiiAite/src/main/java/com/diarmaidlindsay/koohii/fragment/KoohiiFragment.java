bc3c971f3fd9228e5e324baff4b2f2d343f4dcad
Diarmaid Lindsay <diarmaid.lindsay@gmail.com>
2015-12-03 14:50:27 +0900

    Button to open a kanji in Koohii website.

--- KoohiiAite/src/main/java/com/diarmaidlindsay/koohii/fragment/KoohiiFragment.java
+++ KoohiiAite/src/main/java/com/diarmaidlindsay/koohii/fragment/KoohiiFragment.java
@@ -1,6 +1,16 @@
 package com.diarmaidlindsay.koohii.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.diarmaidlindsay.koohii.R;
 
 /**
  * For display of information retrieved from Koohii.
@@ -8,5 +18,27 @@ import android.support.v4.app.Fragment;
  * Favourited stories (if signed in)
  */
 public class KoohiiFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        final String kanji = args.getString("kanji");
 
+        View view = inflater.inflate(R.layout.fragment_detail_koohii, container, false);
+        Button koohiiButton = (Button) view.findViewById(R.id.button_open_koohii);
+        String buttonString = String.format(getString(R.string.button_koohii), kanji);
+        koohiiButton.setText(buttonString);
+
+        koohiiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = String.format("http://kanji.koohii.com/study/kanji/%s", kanji);
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
 }
