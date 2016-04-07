63ed1326c047d0860031e053a7058d389116c540
Diarmaid Lindsay <diarmaid.lindsay@gmail.com>
2016-04-07 12:31:48 +0900

    Fix external storage permissions in Android Marshmallow

--- KoohiiAite/src/main/java/com/diarmaidlindsay/koohii/activity/ImportStoryActivity.java
+++ KoohiiAite/src/main/java/com/diarmaidlindsay/koohii/activity/ImportStoryActivity.java
@@ -1,15 +1,24 @@
 package com.diarmaidlindsay.koohii.activity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.diarmaidlindsay.koohii.R;
 import com.diarmaidlindsay.koohii.adapter.ImportStoryAdapter;
 import com.diarmaidlindsay.koohii.interfaces.OnCSVParseCompleted;
@@ -30,6 +39,9 @@ import java.util.List;
 public class ImportStoryActivity extends AppCompatActivity implements OnDatabaseOperationCompleted, OnCSVParseCompleted {
     static final int ACTIVITY_CODE = 2;
     static final int FILE_CODE = 100;
+    //arbitrary int code for storage permission request
+    static final int REQUEST_READ_STORAGE = 77;
+
     ListView listCSVContent; //the csv file's valid rows
     Button buttonChooseFile; //open file browser
     Button buttonConfirm; //write to database
@@ -51,8 +63,7 @@ public class ImportStoryActivity extends AppCompatActivity implements OnDatabase
         buttonChooseFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(v.getContext(), FileChooserActivity.class);
-                startActivityForResult(i, FILE_CODE);
+                checkPermission();
             }
         });
 
@@ -84,6 +95,58 @@ public class ImportStoryActivity extends AppCompatActivity implements OnDatabase
         buttonConfirm.setEnabled(false);
         buttonCancel.setEnabled(false);
         listAdapter.notifyDataSetChanged();
+    }
+
+    private void checkPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_READ_STORAGE);
+
+            // The callback method gets the result of the request.
+        } else {
+            openFileChooser();
+        }
+    }
+
+    private void openFileChooser() {
+        Intent i = new Intent(this, FileChooserActivity.class);
+        startActivityForResult(i, FILE_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // http://stackoverflow.com/a/32473449/4653788
+                    // Need to restart the process to grant write to SD permissions
+                    // Schedule start after 1 second
+                    PendingIntent pi = PendingIntent.getActivity(
+                            this,
+                            0,
+                            getIntent(),
+                            PendingIntent.FLAG_CANCEL_CURRENT);
+                    AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                    am.set(AlarmManager.RTC, System.currentTimeMillis() + 1000, pi);
+
+                    // Stop now
+                    System.exit(0);
+                } else {
+                    Toast.makeText(this,
+                            "Please grant permission to read External Storage in order to Import CSV",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 
     @Override
@@ -128,7 +191,7 @@ public class ImportStoryActivity extends AppCompatActivity implements OnDatabase
             buttonConfirm.setEnabled(true);
             buttonCancel.setEnabled(true);
             ToastUtil.makeText(this, "CSV file import successful", Toast.LENGTH_SHORT).show();
-            importCount.setText(listAdapter.getCount() + " stories found for import.");
+            importCount.setText(String.format("%d stories found for import.", listAdapter.getCount()));
         } else {
             ToastUtil.makeText(this, "Failed to read CSV file", Toast.LENGTH_LONG).show();
         }
